5417192bf7f9001ec536791cb4901d70cb4ecebf
bjellesma <william.jellesma@gmail.com>
2020-10-16 00:31:29 -0400

    bugfix: unable to login because of env var

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     clientID: 'caolVXfgEL9z2t67IMOhcl10alFoRDQs',
     domain: 'dev-cpb64ukj.us.auth0.com',
     audience: 'https://127.0.0.1:5000',
-    redirect: 'https://vibrant-bardeen-8e238e.netlify.app//callback',
+    redirect: 'https://vibrant-bardeen-8e238e.netlify.app/callback',
     scope: 'openid profile email',
   },
   api_url: 'https://castifyio.herokuapp.com'
